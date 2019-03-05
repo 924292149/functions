308e3192fe173c0758e4c9874b1f1d7df83595dd
max <924292149@qq.com>
2019-03-05 11:43:54 +0800

    this is a release version for jsonQuickFuctions

--- Def.py
+++ Def.py
@@ -3,38 +3,33 @@ import os
 class JsonObj():#json related
 	def __init__(self,jsonFileName):
 		self.jsonFileName = jsonFileName
-		pass
-	
-	def jsAdd(self,jsKey,jsValue):#add a pair of 'key' & 'value' in jsFile
-		jsDict = {}
-		if os.path.exists(self.jsonFileName):
-			with open(self.jsonFileName,'r',encoding='utf-8') as jsFile:
-				jsDict = json.load(jsFile)
-		jsDict[jsKey] = jsValue
-		with open(self.jsonFileName,'w',encoding='utf-8') as jsFile:
-			json.dump(jsDict,jsFile)
-
-	def jsDelete(self,jsKey):
-		jsDict = {}
-		with open(self.jsonFileName,'r',encoding='utf-8') as jsFile:
-			jsDict = json.load(jsFile)
-		del jsDict[jsKey]#need fix error correct
-		with open(self.jsonFileName,'w',encoding='utf-8') as jsFile:
-			json.dump(jsDict,jsFile)
-
-	def jsCheck(self,jsKey):
-		jsDict = {}
-		with open(self.jsonFileName,'r',encoding='utf-8') as jsFile:
-			jsDict = json.load(jsFile)
-		return jsDict[jsKey]
 		
-	def jsCheckAll(self):
+	def jsLoad(self):
 		jsDict = {}
 		with open(self.jsonFileName,'r',encoding='utf-8') as jsFile:
 			jsDict = json.load(jsFile)
 		return jsDict
+	
+	def jsDump(self,jsDict):
+		with open(self.jsonFileName,'w',encoding='utf-8') as jsFile:
+			json.dump(jsDict,jsFile)
+
+	def jsAdd(self,jsKey,jsValue):
+		if os.path.exists(self.jsonFileName):
+			jsDict = self.jsLoad()
+		else:
+			jsDict = {}
+		jsDict[jsKey] = jsValue
+		self.jsDump(jsDict)
 		
+	def jsDelete(self,jsKey):
+		jsDict = self.jsLoad()
+		del jsDict[jsKey]#need fix error correct
+		self.jsDump(jsDict)
+
+	def jsCheck(self,jsKey):
+		jsDict = self.jsLoad()
+		return jsDict[jsKey]
 		
-new = JsonObj('this_is_a_file')
-new.jsAdd('this_is_a_key','this_is_a_value')
-print(new.jsCheckAll())
+class TimeManage():
+	
